74a822e8c51e9a453eaab0b9f160f17b24e94c6c
Jaden <jaden.anthony2@gmail.com>
2025-12-10 18:00:08 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -28,7 +28,7 @@ CREATE TABLE `users` (
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO users VALUES ('Lynne', 'Taylor', 'taylyn73265', 'lynnetaylor@gmail.com', 'user'),
-('Alex', 'Robin', '74fredrick!', 'alrobin@gmail.com', 'user'),
-('Kenzie', 'McDonald', 'ruby4867!', 'kenzie.mcdonald3@gmail.com', 'user');
+INSERT INTO users VALUES (NULL, 'Lynne', 'Taylor', 'taylyn73265', 'lynnetaylor@gmail.com', 'user', NOW(),
+(NULL, 'Alex', 'Robin', '74fredrick!', 'alrobin@gmail.com', 'user', NOW(),
+(NULL, 'Kenzie', 'McDonald', 'ruby4867!', 'kenzie.mcdonald3@gmail.com', 'user', NOW();
 
