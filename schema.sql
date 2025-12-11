530c40d10c4c78d68f608697db193b7de55d54cc
Jaden <jaden.anthony2@gmail.com>
2025-12-10 19:00:05 -0500

    Update schema.sql
    Updated users table, and created contacts table

--- schema.sql
+++ schema.sql
@@ -11,25 +11,45 @@ DROP DATABASE IF EXISTS dolphin_crm;
 CREATE DATABASE dolphin_crm;
 USE dolphin_crm;
 
-
 -- Creation of Tables
 
 -- User Table
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS users;
 CREATE TABLE `users` (
   `id` int NOT NULL auto_increment,
   `firstname` varchar(30) NOT NULL,
   `lastname` varchar(30) NOT NULL,
   `password` varchar(40) NOT NULL,
   `email` varchar(40) NOT NULL,
-  `role` varchar(15) NOT NULL,
+  `role` enum('Admin', 'Member') NOT NULL,
   `date_time` datetime NOT NULL default current_timestamp,
   
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO users (firstname, lastname, password, email, role) VALUES 
-  ('Lynne', 'Taylor', 'taylyn73265', 'lynnetaylor@gmail.com', 'user'),
-  ('Alex', 'Robin', '74fredrick!', 'alrobin@gmail.com', 'user'),
-  ('Kenzie', 'McDonald', 'ruby4867!', 'kenzie.mcdonald3@gmail.com', 'user');
+  ('Alex', 'Robin', 'password123', 'admin@project2.com', 'Admin'),
+  ('Lynne', 'Taylor', 'taylyn73265', 'lynnetaylor@gmail.com', 'Member'),
+  ('Kenzie', 'McDonald', 'ruby4867!', 'kenzie.mcdonald3@gmail.com', 'Member');
+SELECT LAST_INSERT_ID();
 
+-- Contacts Table
+CREATE TABLE `contacts` (
+  `id` int NOT NULL auto_increment,
+  `title` varchar(6) NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `telephone` varchar(15) NOT NULL,
+  `company` varchar(40) NOT NULL,
+  `type` enum('Sales Lead', 'Support') NOT NULL,
+  `assigned_to` int NOT NULL default '0',
+  `created_by` int NOT NULL default '0',
+  `created_at` datetime NOT NULL default current_timestamp,
+  `updated_at` datetime NOT NULL default current_timestamp on update current_timestamp,
+  
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO contacts (title, firstname, lastname, email, telephone, company, type, assigned_to, created_by) VALUES 
+  ('Mrs', 'Gwen', 'Claire', 'gwen.claire@gmail.com', '876-123-4567', RealCompCo, 'Support', last_insert_id(), 1);
