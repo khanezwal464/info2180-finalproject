2e16664acb23b39b392cea408bab5074caf9f3cb
Jaden <jaden.anthony2@gmail.com>
2025-12-27 15:56:27 -0500

    Update function.php

--- function.php
+++ function.php
@@ -1,6 +1,6 @@
 <?php
 
-function check_login($conn){ 
+/*function check_login($conn){ 
     if(isset($_SESSION['email'])){
         $email = $_SESSION['email'];
         $query = "select * from users where email = '$email' limit 1"; 
@@ -20,4 +20,24 @@ function check_login($conn){
     header("Location: UL_session.php"); 
     die;
     }
+}*/
+
+ function check_login($conn) { 
+     if (isset($_SESSION['email'])) { 
+         $email = $_SESSION['email'];             
+         $stmt = $conn->prepare("SELECT * FROM users WHERE email = ? LIMIT 1"); 
+         $stmt->execute([$email]); $user_data = $stmt->fetch(PDO::FETCH_ASSOC); 
+
+        if ($user_data) { 
+            return $user_data; 
+        } 
+    }  
+
+    header("Location: user_login.php"); 
+    exit; 
+
 }
+
+?>
+    
+   
