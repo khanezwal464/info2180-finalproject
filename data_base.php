dfab74c3dbee70e9d6806540290ce6f7591f1525
Jaden <jaden.anthony2@gmail.com>
2025-12-26 22:22:38 -0500

    Update data_base.php

--- data_base.php
+++ data_base.php
@@ -5,7 +5,7 @@ $username = 'finalproj_user';
 $password = 'password123';
 
 try {
-    $pdo = new PDO("mysql:host=$host;port=3307;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $err) {
     echo "Failed to connect to database: " . $err->getMessage();
