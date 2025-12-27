b812bfc3c74ad24b5cb3513e3b605ec770e3d9ad
Jaden <jaden.anthony2@gmail.com>
2025-12-27 18:30:05 -0500

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -35,21 +35,21 @@ $user_data = check_login($conn);
                 <h2>Dashboard</h2>
                 <button id = "newContact">+ Add Contact</button>
             </div>
-            
-            <div id = "filterOp">
-                <img id = "filter" src="filter_icon.png">
-                <label for = "filterOp" id="function">Filter By: </label>
 
-                <p id = "General" class = "filter">All</p>
-                <p id= "Sales" class = "filter">Sales Leads</p>
-                <p id = "Support" class = "filter">Support</p>
-                <p id = "Assigned" class = "filter">Assigned to me</p>
-            </div>
-
-            
-            <div id="result"></div>
+            <div class="table_section">
+                <div id = "filterOp">
+                    <img id = "filter" src="filter_icon.png">
+                    <label for = "filterOp" id="function">Filter By: </label>
     
-
+                    <p id = "General" class = "filter">All</p>
+                    <p id= "Sales" class = "filter">Sales Leads</p>
+                    <p id = "Support" class = "filter">Support</p>
+                    <p id = "Assigned" class = "filter">Assigned to me</p>
+                </div>
+    
+                
+                <div id="result"></div>
+            </div>
         </main>
 
         <?php include 'dolph_nav.php';?>
