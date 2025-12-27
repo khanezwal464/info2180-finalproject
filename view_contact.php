dfab1792e6ce2abbda4d5043a20005f00b86d4c8
Jaden <jaden.anthony2@gmail.com>
2025-12-27 10:41:12 -0500

    Update view_contact.php

--- view_contact.php
+++ view_contact.php
@@ -133,8 +133,9 @@
 			<?php endif; ?>
 
 			<div class="create_note">
+				<p>Add a note for <?= htmlspecialchars($contact['firstname'] . ' ' . $contact['lastname']); ?></p>
+				
 				<div class="note_section">
-					<p>Add a note for <?= htmlspecialchars($contact['firstname'] . ' ' . $contact['lastname']); ?></p>
 					<input type="text" name="new_note" id="new_note" placeholder="Enter your text here">
 				</div>
 				<button type="submit" id="submit">Submit</button>
