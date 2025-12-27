6bc27e2ab5db0b33a84edcd0f7b955a8d9a36e6b
Jaden <jaden.anthony2@gmail.com>
2025-12-27 17:16:07 -0500

    Update filter.php

--- filter.php
+++ filter.php
@@ -1,25 +1,29 @@
 <?php
 session_start();
 
-include "connection.php";
+require "data_base.php";
 include "function.php";
 
-if(isset($_GET['filter'])){
+/*if(isset($_GET['filter'])){
     $query = strip_tags($_GET['filter']);
 }
+
 else{
     $query = '';
-}
+}*/
+
+$query = isset($_GET['filter']) ? strip_tags($_GET['filter']) : '';
 
 if($query == ''){
-    $filt = "SELECT * from contacts";
-
+    /*$filt = "SELECT * from contacts";
     $result = mysqli_query($conn, $filt);
-
-    $results = mysqli_fetch_all($result, MYSQLI_ASSOC);?>
+    $results = mysqli_fetch_all($result, MYSQLI_ASSOC);*/
+    
+    $stmt = $conn->query("SELECT * FROM contacts"); 
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
 
     <table id = "table">
-        
         <tr>
             <th>Name</th>
             <th>Email</th>
@@ -28,33 +32,35 @@ if($query == ''){
             <th></th>
         </tr>
         
-    <?php
-    if($results != null){
-         foreach($results as $row){?>
-        <tr class = "row">
-            <td><strong><?=htmlspecialchars($row['title']." ".$row['firstname']." ".$row['lastname'])?></strong></td>
-            <td class = "email"><?=htmlspecialchars($row['email'])?></td>
-            <td><?=htmlspecialchars($row['company'])?></td>
-            <td class= "type"><?=htmlspecialchars($row['type'])?></td>
-            <td class = "link">View</td>
-        </tr>
-
-    <?php }}?>
-
+    <?php if($results != null): ?>
+         <?php foreach($results as $row): ?>
+            <tr class = "row">
+                <td><strong><?=htmlspecialchars($row['title']." ".$row['firstname']." ".$row['lastname'])?></strong></td>
+                <td class = "email"><?=htmlspecialchars($row['email'])?></td>
+                <td><?=htmlspecialchars($row['company'])?></td>
+                <td class= "type"><?=htmlspecialchars($row['type'])?></td>
+                <td class = "link">View</td>
+            </tr>
+        <?php endforeach; ?>
+    <?php endif; ?>
     </table>
-<?php }
 
-elseif($query == 'Sales'|| $query == 'Support'){
-    $filt = "SELECT * from contacts WHERE type LIKE ?";
+<?php 
+} elseif($query == 'Sales'|| $query == 'Support'){ 
+    /*$filt = "SELECT * from contacts WHERE type LIKE ?";
     $query = "%".strip_tags($_GET['filter'])."%";
     $statement = $conn->prepare($filt);
     $statement ->bind_param("s",$query);
     $statement ->execute();
     $result = $statement ->get_result();
-    $results = $result ->fetch_all(MYSQLI_ASSOC);?>
+    $results = $result ->fetch_all(MYSQLI_ASSOC);?>*/
 
+    $filter = "%".strip_tags($_GET['filter'])."%"; 
+    $stmt = $conn->prepare("SELECT * FROM contacts WHERE type LIKE ?"); 
+    $stmt->execute([$filter]); 
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
     <table>
-        
         <tr>
             <th>Name</th>
             <th>Email</th>
@@ -63,35 +69,36 @@ elseif($query == 'Sales'|| $query == 'Support'){
             <th></th>
         </tr>
         
-    <?php 
-    if($results != null){
-        foreach($results as $row){?>
-        <tr class = "row">
-            <td><strong><?=htmlspecialchars($row['title']." ".$row['firstname']." ".$row['lastname'])?></strong></td>
-            <td class = "email"><?=htmlspecialchars($row['email'])?></td>
-            <td><?=htmlspecialchars($row['company'])?></td>
-            <td class= "type"><?=htmlspecialchars($row['type'])?></td>
-            <td class = "link">View</td>
-        </tr>
-
-    <?php }}?>
-
+    <?php if($results != null): ?>
+        <?php foreach($results as $row): ?>
+            <tr class = "row">
+                <td><strong><?=htmlspecialchars($row['title']." ".$row['firstname']." ".$row['lastname'])?></strong></td>
+                <td class = "email"><?=htmlspecialchars($row['email'])?></td>
+                <td><?=htmlspecialchars($row['company'])?></td>
+                <td class= "type"><?=htmlspecialchars($row['type'])?></td>
+                <td class = "link">View</td>
+            </tr>
+          <?php endforeach; ?>
+        <?php endif; ?>
     </table>
-
-<?php }
-
-elseif($query == 'Assigned'){
-    $sess = strip_tags($_SESSION['id']);
+        
+<?php
+} elseif($query == 'Assigned'){
+    /*$sess = strip_tags($_SESSION['id']);
     $filt = "SELECT * from contacts WHERE contacts.assigned_to = ?";
 
     $statement = $conn->prepare($filt);
     $statement ->bind_param("s",$sess);
     $statement ->execute();
     $result = $statement ->get_result();
-    $results = $result ->fetch_all(MYSQLI_ASSOC);?>
+    $results = $result ->fetch_all(MYSQLI_ASSOC);?>*/
 
+    $sess = $_SESSION['id']; 
+    $stmt = $conn->prepare("SELECT * FROM contacts WHERE assigned_to = ?"); 
+    $stmt->execute([$sess]); 
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
     <table>
-        
         <tr>
             <th>Name</th>
             <th>Email</th>
@@ -100,19 +107,19 @@ elseif($query == 'Assigned'){
             <th></th>
         </tr>
         
-    <?php 
-    if($results != null){
-        foreach($results as $row){?>
-        <tr class = "row">
-            <td><strong><?=htmlspecialchars($row['title']." ".$row['firstname']." ".$row['lastname'])?></strong></td>
-            <td class = "email"><?=htmlspecialchars($row['email'])?></td>
-            <td><?=htmlspecialchars($row['company'])?></td>
-            <td class= "type"><?=htmlspecialchars($row['type'])?></td>
-            <td class = "link">View</td>
-        </tr>
-
-    <?php }}?>
-
+    <?php if($results != null): ?>
+        <?php foreach($results as $row): ?>
+            <tr class = "row">
+                <td><strong><?=htmlspecialchars($row['title']." ".$row['firstname']." ".$row['lastname'])?></strong></td>
+                <td class = "email"><?=htmlspecialchars($row['email'])?></td>
+                <td><?=htmlspecialchars($row['company'])?></td>
+                <td class= "type"><?=htmlspecialchars($row['type'])?></td>
+                <td class = "link">View</td>
+            </tr>
+        <?php endforeach; ?>
+      <?php endif; ?>
     </table>
     
-<?php }
+<?php 
+}
+?>
