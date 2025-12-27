b41e9e2fac30ba795ead6843a9d666682607a811
Jaden <jaden.anthony2@gmail.com>
2025-12-27 12:54:41 -0500

    Update data_base.php

--- data_base.php
+++ data_base.php
@@ -2,19 +2,14 @@
 
 <?php
 
-
  $host = 'localhost';
   $username = 'finalproj_user';
   $password = 'password123';
   $dbname = 'dolphin_crm';
-
-  
+ 
   try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-      
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $stmt = $conn->query("SELECT firstname, lastname, email, role, date_time FROM users");
 
 	} catch (PDOException $e) {
 	  die("Connection failed: " . $e->getMessage());
