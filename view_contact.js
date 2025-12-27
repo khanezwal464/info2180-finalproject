447a60de523ce9e2bc5f57141f918c115aa03fc0
Jaden <jaden.anthony2@gmail.com>
2025-12-27 12:35:14 -0500

    Create view_contact.js

---
+++ view_contact.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+  //Buttons 
+  const submitButton = document.getElementById("submit");
+  const assignButton = document.getElementById("assign");
+  const switchButton = document.getElementById("switch");
+
+
+
+});
