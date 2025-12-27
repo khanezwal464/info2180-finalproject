3dfa3c9b1f09cd25c1a4297d273363cdde3b0f90
khanezwal464 <khanezwallace18@gmail.com>
2025-12-27 00:32:15 -0500

    Updated view_users.php and data_base.php to modularize the implementation.

--- data_base.php
+++ data_base.php
@@ -1,14 +1,24 @@
-<?php
-$host = 'localhost';
-$dbname = 'dolphin_crm';
-$username = 'finalproj_user';
-$password = 'password123';
 
-try {
-    $pdo = new PDO("mysql:host=$host;port=3307;dbname=$dbname;charset=utf8mb4", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $err) {
-    echo "Failed to connect to database: " . $err->getMessage();
+
+<?php
+
+
+ $host = 'localhost';
+  $username = 'finalproj_user';
+  $password = 'password123';
+  $dbname = 'dolphin_crm';
+
+  
+  try {
+    $conn = new PDO("mysql:host=$host;port=3307;dbname=$dbname;charset=utf8mb4", $username, $password);
+      
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $conn->query("SELECT firstname, lastname, email, role, date_time FROM users");
+
+	} catch (PDOException $e) {
+	  die("Connection failed: " . $e->getMessage());
 }
+
 
 ?>
--- view_users.php
+++ view_users.php
@@ -22,24 +22,8 @@
 
 
 <?php
-
-  $host = 'localhost';
-  $username = 'finalproj_user';
-  $password = 'password123';
-  $dbname = 'dolphin_crm';
-
-  
-  try {
-    $conn = new PDO("mysql:host=$host;port=3307;dbname=$dbname;charset=utf8mb4", $username, $password);
-      
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $stmt = $conn->query("SELECT firstname, lastname, email, role, date_time FROM users");
-
-	} catch (PDOException $e) {
-	  die("Connection failed: " . $e->getMessage());
-}
-	?>
+require_once 'data_base.php'; 
+?>
 
 
 	<div class="table">
