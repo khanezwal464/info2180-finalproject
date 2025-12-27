3a26572c72d765c9224afbb41b3b723c7977be14
Jaden <jaden.anthony2@gmail.com>
2025-12-26 22:05:26 -0500

    Update view_users.php

--- view_users.php
+++ view_users.php
@@ -17,10 +17,7 @@
 
 	<div class="container">
 		<div class="sidebar">
-			<a link href="">Home</a>
-			<a link href="">New Contact</a>
-			<a link href="">Users</a>
-			<a link href="">Logout</a>
+			<?php include'dolph_nav.php';?>
 		</div>
 
 
