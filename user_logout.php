630122f6f387430abe81052a78cdd98bc3311614
Jaden <jaden.anthony2@gmail.com>
2025-12-27 21:38:32 -0500

    Update user_logout.php

--- user_logout.php
+++ user_logout.php
@@ -1,8 +1,11 @@
 <?php
   session_start();
+  
+  //Terminating session
   session_unset();
   session_destroy();
   
+  //Redirecting to login page
   header("Location: user_login.php");
   exit;
 ?>
