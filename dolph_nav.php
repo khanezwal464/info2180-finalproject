e9848b821901ea1c356ae91b3022849468e3b665
Jaden <jaden.anthony2@gmail.com>
2025-12-27 19:28:44 -0500

    Update dolph_nav.php

--- dolph_nav.php
+++ dolph_nav.php
@@ -1,6 +1,22 @@
 <nav class="sidebar">
-    <a href="dashboard.php">Home</a>
-    <a href="add_contact.php">New Contact</a>
-    <a href="view_users.php">Users</a>
-    <a href="user_login.php">Logout</a>
+    <div class="nav_item">
+        <img src="home_icon.png" alt="Home" class="nav-icon">
+        <a href="dashboard.php">Home</a>
+    </div>
+
+    <div class="nav_item">
+        <img src="contact_icon.png" alt="Contact" class="nav-icon">
+        <a href="add_contact.php">New Contact</a>
+    </div>
+
+    <div class="nav_item">
+        <img src="users_icon.png" alt="Users" class="nav-icon">
+        <a href="view_users.php">Users</a>
+    </div>
+
+    <div class="nav_item">
+        <img src="logout_icon.png" alt="Logout" class="nav-icon">
+        <a href="user_login.php">Logout</a>
+    </div>
+    
 </nav>
